6164b8191c313f3951d4d2ba7caf65b25eb6884c
Rubens TORRES-LACAZE <rubens.torres-lacaze@telecom-sudparis.eu>
2025-03-03 15:08:53 +0100

    Ajout des arguments à la commande, création et destruction du repo, copie des première versions

--- modules.py
+++ modules.py
@@ -1,17 +1,75 @@
 import os
 import shutil
+import time
+
+
+def home():
+    print('Versionner 1.0')
+
+def unknownCommand():
+    print('Unknown command')
 
 
 
-def init(VERDIR):
+def init(CONFIG):
+    VERDIR = CONFIG['VERDIR']
+    DATAFILE = CONFIG['DATAFILE']
     # Get working directory
     workingDirectory = os.getcwd()
     # Checks if versionner directory exists
     isInit = os.path.isdir(f'{workingDirectory}/{VERDIR}')
     if isInit:
         print('Repo already created in current folder')
+        return
+    os.mkdir(f'{workingDirectory}/{VERDIR}')
+    with open(f'{workingDirectory}/{VERDIR}/{DATAFILE}','w') as file:
+        file.writelines('')
+    print('Repo created in current directory')
+
+def destroy(CONFIG):
+    VERDIR = CONFIG['VERDIR']
+    # Get working directory
+    workingDirectory = os.getcwd()
+    # Checks if versionner directory exists
+    isInit = os.path.isdir(f'{workingDirectory}/{VERDIR}')
+    if isInit:
+        shutil.rmtree(f'{workingDirectory}/{VERDIR}')
+        print('Repo erased')
+        return
     else:
-        os.mkdir(f'{workingDirectory}/{VERDIR}')
-        with open('./ver/hello','w') as file:
-            file.writelines('')
-        print('Repo created in current directory')
+        print('No repo in current directory')
+        return
+    
+
+
+def version(CONFIG):
+    # Checks if versionner directory exists
+    VERDIR = CONFIG['VERDIR']
+    # Get working directory
+    workingDirectory = os.getcwd()
+    # Checks if versionner directory exists
+    isInit = os.path.isdir(f'{workingDirectory}/{VERDIR}')
+    if not isInit:
+        print('No repo in current folder')
+        return
+    # Lists all files and directories
+    objects = os.listdir(workingDirectory)
+    objects.remove(VERDIR)
+    # Creates version ID
+    versionId = time.time()
+    # Creates directory for files in VERDIR
+    versionDirectoryPath = f'{workingDirectory}/{VERDIR}/{versionId}'
+    os.mkdir(versionDirectoryPath)
+    # Copies files in version directory
+    print(objects)
+    for object in objects:
+        objectPath = f'{workingDirectory}/{object}'
+        if os.path.isfile(objectPath):
+            shutil.copy2(objectPath, versionDirectoryPath)
+        if os.path.isdir(objectPath):
+            shutil.copytree(objectPath, f'{versionDirectoryPath}/{object}', copy_function=shutil.copy2)
+    print('Done ! ')
+
+    
+
+
--- versionner.py
+++ versionner.py
@@ -3,32 +3,53 @@ import shutil
 import sys
 import modules
 
-VERDIR = 'ver'
 
-def uk():
-    print('???')
-
-
-# Dictionnaire des fonctions et leur commandes associées
-functionToCommands = {
-    modules.init: ['init', 'i'],
-    uk: []
+CONFIG = {
+    'VERDIR': 'ver',
+    'DATAFILE': 'data'
 }
 
-# Créer un dictionnaire pour mapper chaque commande/alias à sa fonction
-commandMap = {}
-for fonction, commands in functionToCommands.items():
-    for command in commands:
-        commandMap[command] = fonction
 
 
-def main():
-    # Get first arg
-    arg = sys.argv[1]
-    commandMap[arg](VERDIR)
+
+
+def loadCommands():
+    # Dictionnaire des fonctions et leur commandes associées
+    functionToCommands = {
+        modules.init: ['init', 'i'],
+        modules.version: ['commit','com','c','ver','v'],
+        modules.destroy: ['destroy', 'd']
+    }
+
+    # Créer un dictionnaire pour mapper chaque commande/alias à sa fonction
+    commandMap = {}
+    for fonction, commands in functionToCommands.items():
+        for command in commands:
+            commandMap[command] = fonction
+    return commandMap
+
+
+
+
+def main(commandMap):
+    # Gets arguments
+    args = sys.argv
+    # If called without arguments
+    if len(args) == 1:
+        modules.home()
+        return
+    # Gets firt argument
+    command = args[1]
+    if command not in commandMap:
+        modules.unknownCommand()
+        return
+    else:
+        commandMap[command](CONFIG)
+        return
     
 
 
 if __name__ == "__main__":
-    #main()
-    pass
+    commandMap = loadCommands()
+    main(commandMap)
+    
