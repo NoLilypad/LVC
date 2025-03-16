bb87fa49f259f0a7c1c609ea3240aba217538cfd
Rubens TORRES-LACAZE <rubens.torres-lacaze@telecom-sudparis.eu>
2025-03-16 20:31:46 +0100

    Erased dev/first try

--- dev/first-try/command.py
+++
@@ -1,9 +0,0 @@
-import os
-from history import History
-
-class Command:
-    def __init__(self, CONFIG):
-        self.HASH_ALGO = CONFIG['HASH_ALGO']
-        self.version = CONFIG['VERSION']    
-        self.workingDirectory = os.getcwd()
-
